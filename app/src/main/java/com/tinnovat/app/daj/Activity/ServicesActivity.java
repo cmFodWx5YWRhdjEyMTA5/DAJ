8ee046a25602af914db6110651d9052df78fecac
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-06-13 19:05:25 +0530

    services

---
+++ app/src/main/java/com/tinnovat/app/daj/Activity/ServicesActivity.java
@@ -0,0 +1,17 @@
+package com.tinnovat.app.daj.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tinnovat.app.daj.BaseActivity;
+import com.tinnovat.app.daj.R;
+
+public class ServicesActivity extends BaseActivity
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services);
+    }
+}
