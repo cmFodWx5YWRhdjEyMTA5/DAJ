132c680aa8a736715f37edaa530a4667e45b1308
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-06-14 18:03:18 +0530

    event

---
+++ app/src/main/java/com/tinnovat/app/daj/Activity/OrderTaxiActivity.java
@@ -0,0 +1,16 @@
+package com.tinnovat.app.daj.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tinnovat.app.daj.BaseActivity;
+import com.tinnovat.app.daj.R;
+
+public class OrderTaxiActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_taxi);
+    }
+}
