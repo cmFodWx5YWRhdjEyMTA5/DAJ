18c64d07aed63abf463c84fdd2fe39a2b11cb173
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-06-21 19:07:29 +0530

    calender

--- app/src/main/java/com/tinnovat/app/daj/Activity/EventBookingActivity.java
+++ app/src/main/java/com/tinnovat/app/daj/Activity/EventBookingActivity.java
@@ -1,7 +1,11 @@
 package com.tinnovat.app.daj.Activity;
 
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 import com.tinnovat.app.daj.BaseActivity;
 import com.tinnovat.app.daj.R;
@@ -10,10 +14,13 @@ import java.util.Objects;
 
 public class EventBookingActivity extends BaseActivity {
 
+    CalendarView cal;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_booking);
         Objects.requireNonNull(getSupportActionBar()).setTitle("EVENT BOOKING");
+        cal = findViewById(R.id.simpleCalendarView);
     }
 }
