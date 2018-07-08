4b61c0f749caa3a5efd1096ec2f4ff52e17da19d
rahultinnovat <rahul.kr@tinnovat.com>
2018-07-08 23:57:38 +0530

    retrofit models nd interface

--- app/src/main/java/com/tinnovat/app/daj/data/network/ApiInterface.java
+++ app/src/main/java/com/tinnovat/app/daj/data/network/ApiInterface.java
@@ -10,6 +10,7 @@ import com.tinnovat.app.daj.data.network.model.ResponseModel;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
@@ -17,5 +18,10 @@ public interface ApiInterface {
     //Login
     @POST("/abc/loginservice/login")
     Call<ResponseModel> login(@Body RequestParams.LoginReequest params);
+    
+
+    //Get Something
+    @POST("/abc/loginservice/login")
+    Call<ResponseModel> getSomething(@Header("Authorization") String authKey, @Body RequestParams.LoginReequest params);
 
 }
